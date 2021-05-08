618fe80d9b2f82123e820a9e12e98dd5e913df15
Steve Perry <ideas@steveperrycreative.com>
2021-05-08 19:23:59 +0100

    Bringing files up to date

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
 
-class App extends React.Component {
+class Router extends React.Component {
     render() {
         return (
             <div className="catch-of-the-day">
